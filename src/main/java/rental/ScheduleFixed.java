e0b680d3f37dba4d7b9e1df4d7228d9a6d7594cb
inho-oh <dhdlsgh111@naver.com>
2020-07-14 14:14:59 +0900

    second

---
+++ src/main/java/rental/ScheduleFixed.java
@@ -0,0 +1,39 @@
+
+package rental;
+
+public class ScheduleFixed extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private String checkDate;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public String getCheckDate() {
+        return checkDate;
+    }
+
+    public void setCheckDate(String checkDate) {
+        this.checkDate = checkDate;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
