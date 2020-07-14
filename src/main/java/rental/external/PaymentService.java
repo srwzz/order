181663c8b5f2ebc460d6cb249ebd5cee6848946b
inho-oh <dhdlsgh111@naver.com>
2020-07-14 17:46:03 +0900

    second

--- src/main/java/rental/external/PaymentService.java
+++ src/main/java/rental/external/PaymentService.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name="Payment", url="http://localhost:8085")
-//@FeignClient(name="Payment", url="http://Payment:8080")
+@FeignClient(name="Payment", url="http://Payment:8080")
 public interface PaymentService {
 
     @RequestMapping(method= RequestMethod.POST, path="/payments")
